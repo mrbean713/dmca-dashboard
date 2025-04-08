f708022a02ff813dacb9fbd7af8f6025594723e4
Nick <rsnick06@yahoo.com>
2025-04-07 23:38:32 -0700

    Disable TypeScript 'no-explicit-any' rule for demo

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+// .eslintrc.js
+module.exports = {
+    extends: ['next', 'next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  }
+  
